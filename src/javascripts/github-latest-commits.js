83338651c9ffe0c3c746b18b1f3459f2c930e159
Rodrigo Ramírez Norambuena <a@rodrigoramirez.com>
2015-11-07 18:40:29 -0300

    Add author param for get commits from a determined author on the project

--- src/javascripts/github-latest-commits.js
+++ src/javascripts/github-latest-commits.js
@@ -23,34 +23,40 @@ $.extend({
 });
 
 $(function() {
-  var branch, callback, container, limit, params, repo, url, username;
+  var author, branch, callback, container, limit, params, repo, title, url, username;
   params = $.getUrlVars();
   username = params.username;
   repo = params.repo;
   limit = params.limit;
   branch = params.branch;
+  author = params.author;
   container = $('#latest-commits-widget');
   callback = function(response) {
-    var index, items, result, ul, _results;
+    var index, items, result, results, ul;
     items = response.data;
     ul = $('#commit-history');
     ul.empty();
-    _results = [];
+    results = [];
     for (index in items) {
       result = items[index];
-      _results.push((function(index, result) {
+      results.push((function(index, result) {
         if (result.author != null) {
           return ul.append("<li class=\"clearfix\">\n  <div class=\"left\">\n    <img class=\"commit-avatar\" src=\"" + result.author.avatar_url + "\">\n  </div>\n  <div class=\"commit-author-info left\">\n      <a href=\"https://github.com/" + result.author.login + "\"><b class=\"commit-author\">" + result.author.login + "</b></a>\n      <br />\n      <b class=\"commit-date\">" + ($.timeago(result.commit.committer.date)) + "</b><br /><i class=\"commit-sha\">SHA: " + result.sha + "</i>\n      <br />\n      <a class=\"commit-message\" href=\"https://github.com/" + username + "/" + repo + "/commit/" + result.sha + "\" target=\"_blank\">" + result.commit.message + "</a>\n  </div>\n</li>");
         }
       })(index, result));
     }
-    return _results;
+    return results;
   };
-  container.find('h4').text("Latest Commits to " + username + "/" + repo);
+  title = "Latest Commits to " + username + "/" + repo;
   url = "https://api.github.com/repos/" + username + "/" + repo + "/commits?callback=callback";
   if (params.branch != null) {
     url += "&sha=" + branch;
   }
+  if (params.author != null) {
+    url += "&author=" + author;
+    title += " by " + author;
+  }
+  container.find('h4').text(title);
   return $.ajax(url, {
     data: {
       per_page: limit
